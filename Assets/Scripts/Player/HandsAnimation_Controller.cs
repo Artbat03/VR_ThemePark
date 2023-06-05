9656ec9fce4e71270e14744294b23ce66fcf23de
Artbat03 <artbat03@gmail.com>
2023-06-05 22:08:06 +0200

    Hands Animations

---
+++ Assets/Scripts/Player/HandsAnimation_Controller.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandsAnimation_Controller : MonoBehaviour
+{
+    // Variables
+    [Header("INPUT ACTION CONTROLLERS")]
+    [SerializeField] private InputActionReference gripInput;
+    [SerializeField] private InputActionReference triggerInput;
+
+    // Private variables
+    private Animator _anim;
+    private float grip;
+    private float trigger;
+
+    private void Awake()
+    {
+        // Getting components
+        _anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        GetInputs();
+    }
+
+    public void GetInputs()
+    {
+        // Reading values
+        grip = gripInput.action.ReadValue<float>();
+        trigger = triggerInput.action.ReadValue<float>();
+        
+        // Assigning values to the animator parameters
+        _anim.SetFloat("Grip", grip);
+        _anim.SetFloat("Trigger", trigger);
+    }
+}
